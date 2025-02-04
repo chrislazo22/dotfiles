702542bdf8bba13a84ee9c18dfd386a0811a3ee1
Chris Lazo <lazochristian.cl@gmail.com>
2025-02-04 08:25:07 -0700

    Push updates

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -9,39 +9,15 @@ return {
 				vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
 			end
 
-			-- Navigation
-			map("n", "]g", gs.next_hunk, "Next Hunk")
-			map("n", "[g", gs.prev_hunk, "Prev Hunk")
-
 			-- Actions
-			map("n", "<leader>gs", gs.stage_hunk, "Stage hunk")
-			map("n", "<leader>gr", gs.reset_hunk, "Reset hunk")
-			map("v", "<leader>gs", function()
-				gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end, "Stage hunk")
-			map("v", "<leader>gr", function()
-				gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end, "Reset hunk")
-
-			map("n", "<leader>gS", gs.stage_buffer, "Stage buffer")
-			map("n", "<leader>gR", gs.reset_buffer, "Reset buffer")
-
-			map("n", "<leader>gu", gs.undo_stage_hunk, "Undo stage hunk")
-
-			map("n", "<leader>gp", gs.preview_hunk, "Preview hunk")
-
 			map("n", "<leader>gb", function()
 				gs.blame_line({ full = true })
 			end, "Blame line")
 			map("n", "<leader>gB", gs.toggle_current_line_blame, "Toggle line blame")
-
-			map("n", "<leader>gd", gs.diffthis, "Diff this")
-			map("n", "<leader>gD", function()
-				gs.diffthis("~")
-			end, "Diff this ~")
-
-			-- Text object
-			map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "Gitsigns select hunk")
+			-- map("n", "<leader>gd", gs.diffthis, "Diff this")
+			-- map("n", "<leader>gD", function()
+			-- 	gs.diffthis("~")
+			-- end, "Diff this ~")
 		end,
 	},
 }
